6d2c925fac632a8c34012c699d09de2c2c6751c8
stephanp <stephanpopescu123@gmail.com>
2025-11-17 10:01:41 -0500

    new tests

--- locustfile.py
+++ locustfile.py
@@ -6,24 +6,14 @@ import app
 
 class ComputeUser(User):
 
- 
-
     @task
-
     def test_compute_fib_value(self):
-
-        start = time.time()
-
- 
+        start = time.time() 
 
         try:
 
             result = app.compute_fib_value()
-
- 
-
             total_ms = (time.time() - start) * 1000
-
             self.environment.runner.stats.log_request(
 
                 "function",
@@ -33,21 +23,13 @@ class ComputeUser(User):
                 total_ms,
 
                 0
-
             )
-
- 
 
         except Exception as e:
 
             total_time = (time.time() - start) * 1000
-
             self.environment.runner.stats.log_error(
-
                 "function",
-
                 "compute_fib_value",
-
                 e
-
             )
